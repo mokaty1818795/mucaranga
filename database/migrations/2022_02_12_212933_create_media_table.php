a7f8bb13200031becd6030e54150ad8f156d819b
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-02-12 21:35:10 +0200

    Media library added and queue jobs for permance on upload images

--- database/migrations/2022_02_12_212933_create_media_table.php
+++ database/migrations/2022_02_12_212933_create_media_table.php
@@ -1,23 +1,18 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 class CreateMediaTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
     public function up()
     {
         Schema::create('media', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('model_type');
-            $table->unsignedBigInteger('model_id');
-            $table->char('uuid', 36)->nullable()->unique();
+
+            $table->morphs('model');
+            $table->uuid('uuid')->nullable()->unique();
             $table->string('collection_name');
             $table->string('name');
             $table->string('file_name');
@@ -30,19 +25,8 @@ class CreateMediaTable extends Migration
             $table->json('generated_conversions');
             $table->json('responsive_images');
             $table->unsignedInteger('order_column')->nullable()->index();
-            $table->timestamps();
 
-            $table->index(['model_type', 'model_id']);
+            $table->nullableTimestamps();
         });
-    }
-
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('media');
     }
 }

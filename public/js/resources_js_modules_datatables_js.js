78b2c6ff13731788e6f70d64c1688f0309bfb052
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-02-16 12:23:57 +0200

    plugins added on project

---
+++ public/js/resources_js_modules_datatables_js.js
@@ -0,0 +1,32 @@
+(self["webpackChunk"] = self["webpackChunk"] || []).push([["resources_js_modules_datatables_js"],{
+
+/***/ "./resources/js/modules/datatables.js":
+/*!********************************************!*\
+  !*** ./resources/js/modules/datatables.js ***!
+  \********************************************/
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+__webpack_require__.e(/*! import() */ "node_modules_jszip_dist_jszip_min_js").then(__webpack_require__.t.bind(__webpack_require__, /*! jszip */ "./node_modules/jszip/dist/jszip.min.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_pdfmake_build_pdfmake_js").then(__webpack_require__.t.bind(__webpack_require__, /*! pdfmake */ "./node_modules/pdfmake/build/pdfmake.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-autofill-bs5_js_autoFill_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-autofill-bs5 */ "./node_modules/datatables.net-autofill-bs5/js/autoFill.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-buttons-bs5_js_buttons_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-buttons-bs5/js/buttons.bootstrap5 */ "./node_modules/datatables.net-buttons-bs5/js/buttons.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-buttons_js_buttons_colVis_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-buttons/js/buttons.colVis.js */ "./node_modules/datatables.net-buttons/js/buttons.colVis.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-buttons_js_buttons_html5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-buttons/js/buttons.html5.js */ "./node_modules/datatables.net-buttons/js/buttons.html5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-buttons_js_buttons_print_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-buttons/js/buttons.print.js */ "./node_modules/datatables.net-buttons/js/buttons.print.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-colreorder-bs5_js_colReorder_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-colreorder-bs5/js/colReorder.bootstrap5 */ "./node_modules/datatables.net-colreorder-bs5/js/colReorder.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-datetime_dist_dataTables_dateTime_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-datetime */ "./node_modules/datatables.net-datetime/dist/dataTables.dateTime.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-fixedcolumns-bs5_js_fixedColumns_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5 */ "./node_modules/datatables.net-fixedcolumns-bs5/js/fixedColumns.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-fixedheader-bs5_js_fixedHeader_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-fixedheader-bs5/js/fixedHeader.bootstrap5 */ "./node_modules/datatables.net-fixedheader-bs5/js/fixedHeader.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-keytable-bs5_js_keyTable_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-keytable-bs5 */ "./node_modules/datatables.net-keytable-bs5/js/keyTable.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-responsive-bs5_js_responsive_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-responsive-bs5 */ "./node_modules/datatables.net-responsive-bs5/js/responsive.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-rowgroup-bs5_js_rowGroup_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-rowgroup-bs5/js/rowGroup.bootstrap5 */ "./node_modules/datatables.net-rowgroup-bs5/js/rowGroup.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-rowreorder-bs5_js_rowReorder_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-rowreorder-bs5 */ "./node_modules/datatables.net-rowreorder-bs5/js/rowReorder.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-scroller-bs5_js_scroller_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-scroller-bs5 */ "./node_modules/datatables.net-scroller-bs5/js/scroller.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-searchbuilder-bs5_js_searchBuilder_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-searchbuilder-bs5/js/searchBuilder.bootstrap5 */ "./node_modules/datatables.net-searchbuilder-bs5/js/searchBuilder.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-searchpanes-bs5_js_searchPanes_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-searchpanes-bs5 */ "./node_modules/datatables.net-searchpanes-bs5/js/searchPanes.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-select-bs5_js_select_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-select-bs5/js/select.bootstrap5 */ "./node_modules/datatables.net-select-bs5/js/select.bootstrap5.js", 23));
+__webpack_require__.e(/*! import() */ "node_modules_datatables_net-staterestore-bs5_js_stateRestore_bootstrap5_js").then(__webpack_require__.t.bind(__webpack_require__, /*! datatables.net-staterestore-bs5/js/stateRestore.bootstrap5 */ "./node_modules/datatables.net-staterestore-bs5/js/stateRestore.bootstrap5.js", 23));
+
+/***/ })
+
+}]);

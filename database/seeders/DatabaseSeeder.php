b62f4615bc75fa80eda6f610338f23bc178d4610
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-03-05 15:22:07 +0200

    Role on User view bug fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,8 +25,12 @@ class DatabaseSeeder extends Seeder
         $root = User::where('email','administrator@ecm.com')->first();
         $director = User::where('email','director@ecm.com')->first();
         $instructor  = User::where('email', 'instrutor@ecm.com')->first();
+        $employee  = User::where('email', 'employee@ecm.com')->first();
+        $dafault  = User::where('email', 'default@ecm.com')->first();
         $root->assignRole(['Root']);
         $director->assignRole(['Director']);
         $instructor->assignRole(['Instructor']);
+        $employee->assignRole(['Employee']);
+        $dafault->assignRole(['Default']);
     }
 }

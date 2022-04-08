df3cfad10a0f75d5041e0a602ad0ba4e60d6bfcc
Nelson  Mutane <Nelson.mutane@uzambeze.ac.mz>
2022-04-08 18:34:33 +0200

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,24 +17,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(RolesTableSeeder::class);
-        // $this->call(CivilStatesTableSeeder::class);
-        // $this->call(VeicleClassesTableSeeder::class);
-        // $this->call(PeriodsTableSeeder::class);
-        // $this->call(PaymentPhasesTableSeeder::class);
-        // $this->call(DocumentTypeSeeder::class);
-        // $root = User::where('email','administrator@ecm.com')->first();
-        // $director = User::where('email','director@ecm.com')->first();
-        // $instructor  = User::where('email', 'instrutor@ecm.com')->first();
-        // $employee  = User::where('email', 'employee@ecm.com')->first();
-        // $dafault  = User::where('email', 'default@ecm.com')->first();
-        // $root->assignRole(['Root']);
-        // $director->assignRole(['Director']);
-        // $instructor->assignRole(['Instructor']);
-        // $employee->assignRole(['Employee']);
-        // $dafault->assignRole(['Default']);
+        $this->call(UsersTableSeeder::class);
+        $this->call(RolesTableSeeder::class);
+        $this->call(CivilStatesTableSeeder::class);
+        $this->call(VeicleClassesTableSeeder::class);
+        $this->call(PeriodsTableSeeder::class);
+        $this->call(PaymentPhasesTableSeeder::class);
+        $this->call(DocumentTypeSeeder::class);
+        $root = User::where('email','administrator@ecm.com')->first();
+        $director = User::where('email','director@ecm.com')->first();
+        $instructor  = User::where('email', 'instrutor@ecm.com')->first();
+        $employee  = User::where('email', 'employee@ecm.com')->first();
+        $dafault  = User::where('email', 'default@ecm.com')->first();
+        $root->assignRole(['Root']);
+        $director->assignRole(['Director']);
+        $instructor->assignRole(['Instructor']);
+        $employee->assignRole(['Employee']);
+        $dafault->assignRole(['Default']);
 
-        Student::factory()->count(40)->has(Account::factory()->count(1))->create();
+        Student::factory()->count(120)->has(Account::factory()->count(1))->create();
     }
 }
